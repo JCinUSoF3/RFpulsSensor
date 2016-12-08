d21d12aa03ceb4293673057e481ea7b9baa4c746
JCinUSoF3 <jco3@mail.usf.edu>
2016-12-08 18:32:01 -0500

    Add files via upload

---
+++ PulseSensorAmped_Arduino_1dot4/PulseSensorAmped_Arduino_1dot4.ino
@@ -0,0 +1,115 @@
+
+/*  Pulse Sensor Amped 1.4    by Joel Murphy and Yury Gitman   http://www.pulsesensor.com
+
+----------------------  Notes ----------------------  ---------------------- 
+This code:
+1) Blinks an LED to User's Live Heartbeat   PIN 13
+2) Fades an LED to User's Live HeartBeat
+3) Determines BPM
+4) Prints All of the Above to Serial
+
+Read Me:
+https://github.com/WorldFamousElectronics/PulseSensor_Amped_Arduino/blob/master/README.md   
+ ----------------------       ----------------------  ----------------------
+*/
+#include <VirtualWire.h>
+
+char b[2];
+String str;
+
+
+//  Variables
+int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+int blinkPin = 13;                // pin to blink led at each beat
+int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
+int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
+
+// Volatile Variables, used in the interrupt service routine!
+volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded! 
+volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat". 
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+
+// Regards Serial OutPut  -- Set This Up to your needs
+static boolean serialVisual = false;   // Set to 'false' by Default.  Re-set to 'true' to see Arduino Serial Monitor ASCII Visual Pulse 
+
+
+void setup(){
+  pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
+  pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
+  Serial.begin(9600);             // we agree to talk fast!
+  interruptSetup();
+
+  vw_set_tx_pin(12);
+  vw_set_ptt_pin(3);
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(1000);   // Bits per sec
+  
+  // sets up to read Pulse Sensor signal every 2mS 
+   // IF YOU ARE POWERING The Pulse Sensor AT VOLTAGE LESS THAN THE BOARD VOLTAGE, 
+   // UN-COMMENT THE NEXT LINE AND APPLY THAT VOLTAGE TO THE A-REF PIN
+//   analogReference(EXTERNAL);   
+}
+
+
+//  Where the Magic Happens
+/*void loop(){
+  
+    serialOutput() ;       
+    
+  if (QS == true){     // A Heartbeat Was Found
+                       // BPM and IBI have been Determined
+                       // Quantified Self "QS" true when arduino finds a heartbeat
+        fadeRate = 255;         // Makes the LED Fade Effect Happen
+                                // Set 'fadeRate' Variable to 255 to fade LED with pulse
+        serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.     
+        QS = false;                      // reset the Quantified Self flag for next time    
+  }
+     
+  ledFadeToBeat();                      // Makes the LED Fade Effect Happen 
+  delay(20);                             //  take a break
+}*/
+
+
+void loop()
+{
+  digitalWrite(7, HIGH); // Flash a light to show transmitting
+  readSensor();
+  vw_send((uint8_t *)b, 2);
+  vw_wait_tx(); // Wait until the whole message is gone
+  
+  digitalWrite(7, LOW);
+  delay(2000);
+}
+
+//  Where the Magic Happens
+void readSensor(){
+  
+    //serialOutput() ;       
+    
+  if (QS == true){     // A Heartbeat Was Found
+                       // BPM and IBI have been Determined
+                       // Quantified Self "QS" true when arduino finds a heartbeat
+         
+                               // Set 'fadeRate' Variable to 255 to fade LED with pulse
+        Serial.print(BPM);
+        str = String(BPM);
+        str.toCharArray(b,2);
+        QS = false;                      // reset the Quantified Self flag for next time    
+  }
+   //vw_tx_active();  
+  //ledFadeToBeat();                      // Makes the LED Fade Effect Happen 
+  delay(20);                             //  take a break
+}
+
+
+/*void ledFadeToBeat(){
+    fadeRate -= 15;                         //  set LED fade value
+    fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
+    analogWrite(fadePin,fadeRate);          //  fade LED
+  }*/
+
+
+
+
